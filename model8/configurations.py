b8f65a883cfa54491ac22cb39e70966ab5297693
Simina Cotop <simina.cotop@gmail.com>
2020-04-15 14:57:40 +0200

    Remove InScript-related code

--- model8/configurations.py
+++ model8/configurations.py
@@ -23,7 +23,6 @@ class Config(object):
     Sample_path = 'outputs/' + Run_Index + '/samples_' + Run_Index
     # for unix
     # LOG_DIR = r'./LOG'
-    #Word2Vec_Google_Path = r'D:\data\GoogleNews-vectors-negative300.bin'
     Word2Vec_Google_Path = r'/home/simina/GoogleNews-vectors-negative300.bin'
     W2V_wrapper_path = os.path.join('.', 'w2v10')
 
@@ -39,10 +38,7 @@ class Config(object):
                          'women_work_sector',
                          'young_evenings'
                          ]
-    # Effective_Scripts = ['bath', 'grocery', 'tree']
-    #Effective_Scripts = ['bath', 'bicycle', 'bus', 'cake', 'flight', 'grocery', 'haircut', 'library', 'train', 'tree']
     
-    #All_Scripts = ['bath', 'bicycle', 'bus', 'cake', 'flight', 'grocery', 'haircut', 'library', 'train', 'tree']
     All_Scripts = [
                          'gender_pay_gap',
                          'median_salary_se',
@@ -125,9 +121,6 @@ class Config(object):
 
     class Seed(object):
         def __init__(self, x_context, e_p_context, e_f_context, agenda):
-            #self.scripts = ['bath', 'bicycle', 'bus', 'cake', 'flight',
-            #                'grocery', 'haircut', 'library', 'train', 'tree']
-            #print (agenda[0])
 
             self.scripts = [ 
                           'gender_pay_gap',
@@ -149,9 +142,6 @@ class Config(object):
             #print (self.scripts)
             self.script = [s for s in self.scripts if self.agenda[0].find(s) != -1][0]
 
-    #x_context = 'after playing football with my friends , ' \
-    #            'i was all sweaty and dirty , ' \
-    #            'so i decided i should take a bath .'.split()
     #x_context = 'this shows gender pay gap'.split()
     x_context = 'the chart shows gender pay gap'.split()
     e_p_context = ['Story_Begin_gender_pay_gap'] * 22 + ['<topic>_gender_pay_gap\n']# ['<Evoking_gender_pay_gap>\n']
@@ -160,21 +150,6 @@ class Config(object):
 
     # agenda includes the events to be instantiated. It begins with the last event that was instantiated
     # in the seed.
-    #agenda_bath = list(['Evoking_bath', 'ScrEv_close_drain', 'ScrEv_turn_water_on',
-    #                    'ScrEv_check_temp', 'ScrEv_undress', 'ScrEv_sink_water', 'ScrEv_relax',
-    #                    'ScrEv_wash', 'ScrEv_get_out_bath', 'ScrEv_dry', 'Story_End_bath'])
-
-    #agenda_bathu = list(['Evoking_bath', 'UnrelEv_bath', 'ScrEv_close_drain', 'UnrelEv_bath',
-    #                    'ScrEv_turn_water_on', 'UnrelEv_bath',
-    #                    'ScrEv_check_temp', 'UnrelEv_bath',
-    #                    'ScrEv_undress', 'UnrelEv_bath',
-    #                    'ScrEv_sink_water', 'UnrelEv_bath',
-    #                    'ScrEv_relax', 'UnrelEv_bath',
-    #                    'ScrEv_wash', 'UnrelEv_bath',
-    #                    'ScrEv_get_out_bath', 'UnrelEv_bath',
-    #                    'ScrEv_dry', 'UnrelEv_bath',
-    #                    'Story_End_bath'])
-    
     agenda_gender = list([#'Story_Begin_gender_pay_gap\n',
                          '<topic>_gender_pay_gap\n',
                          '<x_axis_label_highest_value>_gender_pay_gap\n', 
@@ -201,52 +176,4 @@ class Config(object):
     #agenda_money = list(['<gender>'])
     Sample_Seed_gender = Seed(x_context, e_p_context, e_f_context, agenda_gender)
 
-    #x_context2 = 'yesterday i went+grocery+shopping .'.split()
-    #e_p_context2 = ['Story_Begin_grocery'] * 3 + ['Evoking_grocery']
-    #e_f_context2 = ['Evoking_grocery'] * 3 + ['ScrEv_take_bags']
-    #agenda_grocery = list(['Evoking_grocery', 'ScrEv_take_bags', 'ScrEv_take_shop_cart', 'ScrEv_enter',
-    #                       'ScrEv_check_list', 'ScrEv_move_section', 'ScrEv_get_groceries', 'ScrEv_get_groceries',
-    #                       'ScrEv_check_off', 'ScrEv_go_checkout', 'ScrEv_wait', 'ScrEv_put_conveyor',
-    #                       'ScrEv_cashier_scan/weight', 'ScrEv_pack_groceries', 'ScrEv_pay', 'ScrEv_get_receipt',
-    #                       'ScrEv_bring_vehicle', 'ScrEv_leave', 'Story_End_grocery'])
-    #Sample_Seed_grocery = Seed(x_context2, e_p_context2, e_f_context2, agenda_grocery)
-
-    #x_contextb2 = 'after playing football with my friends , '.split()
-    #e_p_contextb2 = ['Story_Begin_bath'] * 7
-    #e_f_contextb2 = ['Evoking_bath'] * 7
-    # agenda includes the events to be instantiated. It begins with the last event that was instantiated
-    # in the seed.
-    #agenda_bath = list(['Story_Begin_bath', 'Evoking_bath', 'ScrEv_close_drain', 'ScrEv_turn_water_on',
-    #                    'ScrEv_check_temp', 'ScrEv_undress', 'ScrEv_sink_water', 'ScrEv_relax',
-    #                    'ScrEv_wash', 'ScrEv_get_out_bath', 'ScrEv_dry', 'Story_End_bath'])
-
-    #Sample_Seed_bath2 = Seed(x_contextb2, e_p_contextb2, e_f_contextb2, agenda_bath)
-
-    #x_contextbb = 'after playing football with my friends , '.split()
-    #e_p_contextbb = ['Story_Begin_bath'] * 7
-    #e_f_contextbb = ['Evoking_bath'] * 7
-    # agenda includes the events to be instantiated. It begins with the last event that was instantiated
-    # in the seed.
-    #agenda_bathbb = list(['Story_Begin_bath', 'Evoking_bath', 'ScrEv_get_towel', 'ScrEv_take_clean_clothes',
-    #                      'ScrEv_turn_water_on', 'ScrEv_turn_water_off',
-    #                      'ScrEv_undress', 'ScrEv_sink_water', 'ScrEv_apply_soap',
-    #                      'ScrEv_wash', 'ScrEv_get_out_bath', 'ScrEv_dry', 'ScrEv_leave', 'Story_End_bath'])
-
-    #x_contextgg = 'yesterday i went+grocery+shopping .'.split()
-    #e_p_contextgg = ['Story_Begin_grocery'] * 3 + ['Evoking_grocery']
-    #e_f_contextgg = ['Evoking_grocery'] * 3 + ['ScrEv_take_bags']
-    #agenda_grocerygg = list(['Evoking_grocery', 'ScrEv_take_bags', 'ScrEv_enter',
-    #                       'ScrEv_check_list', 'ScrEv_get_groceries',
-    #                       'ScrEv_check_off', 'ScrEv_go_checkout', 'ScrEv_wait', 'ScrEv_put_conveyor',
-    #                       'ScrEv_cashier_scan/weight', 'ScrEv_pack_groceries', 'ScrEv_pay', 'ScrEv_get_receipt',
-    #                       'ScrEv_bring_vehicle', 'ScrEv_return_shop_cart', 'ScrEv_leave', 'Story_End_grocery'])
-
-    #1Sample_Seed_grocerygg = Seed(x_contextgg, e_p_contextgg, e_f_contextgg, agenda_gender)
-    #Sample_Seed_grocerygg = Seed(x_contextgg, e_p_contextgg, e_f_contextgg, agenda_grocerygg)
-
-    #Sample_Seed_bathb = Seed(x_contextbb, e_p_contextbb, e_f_contextbb, agenda_bathbb)
-
-    #Sample_seed_bathu = Seed(x_contextb2, e_p_contextb2, e_f_contextb2, agenda_bathu)
-
-    #Seeds = [Sample_Seed_bath2, Sample_Seed_grocery, Sample_Seed_bathb, Sample_Seed_grocerygg]
     Seeds = [Sample_Seed_gender]
