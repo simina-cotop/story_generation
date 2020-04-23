5d9f22bd58eec4a570430cce0d733de640ae3a47
Simina Cotop <simina.cotop@gmail.com>
2020-04-23 14:25:43 +0200

    Finish first sentences script

--- get_charts_prompt.py
+++ get_charts_prompt.py
@@ -9,24 +9,41 @@ def cleanhtml(raw_html):
   cleantext = re.sub(cleanr, '', raw_html)
   return cleantext
 
-files = [file for file in glob.glob("*.txt")]
-files=['Number_of_top_Unis.txt']
-print(files)
 
-first_sentences = []
+def get_files():
+    files = [file for file in glob.glob("*.txt")]
+    #files=['Number_of_top_Unis.txt']
+    print(files)
+    return files
 
-for file in files:
-    with open(file, 'r', encoding='latin1') as f:
-        data = f.read()
-        descriptions = data.split('\"')
-        descriptions = [i for i in descriptions if (i != '\n') and (i != '\n\n\n') and (i != '\n\n\n\n')]
-        print(descriptions[:10],'\n')
-        for desc_idx in range(0,len(descriptions)):
-            sentence = descriptions[desc_idx].split('.')
-            first_sentences.append(cleanhtml(sentence[0]))
 
-print(len(first_sentences))
-with open('output.txt', 'w', encoding='latin1') as g:
-    for sent in first_sentences:
-        g.write(sent+'\n')
-g.close()
+def get_first_sentences(files):
+    all_files = {}
+
+    for file in files:
+        first_sentences = []
+        with open(file, 'r', encoding='latin1') as f:
+            data = f.read()
+            descriptions = data.split('"')
+            descriptions = [i for i in descriptions if (i != '\n') and (i != '\n\n\n') and (i != '\n\n\n\n')]
+            print(descriptions[:10],'\n')
+            for desc_idx in range(0,len(descriptions)):
+                sentence = descriptions[desc_idx].split('.')
+                first_sent = sentence[0]
+                first_sent = cleanhtml(first_sent)
+                first_sent = re.sub(r"\s+", " ", first_sent)
+                #print('first_sent=',first_sent,'\n')
+                first_sentences.append(first_sent)
+        all_files[file] = first_sentences
+    #print(all_files)
+
+    '''print(len(first_sentences))
+    with open('output.txt', 'w', encoding='latin1') as g:
+        for sent in first_sentences:
+            g.write(sent)
+    g.close()'''
+
+
+if __name__ == '__main__':
+    files = get_files()
+    get_first_sentences(files)
