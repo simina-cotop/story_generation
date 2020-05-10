5f8594a1c9891ad0890664c67e462d3d86cab40e
Simina Cotop <simina.cotop@gmail.com>
2020-05-10 15:05:00 +0200

    Generate input files for rouge

--- gpt2/parse_gpt2_output.py
+++ gpt2/parse_gpt2_output.py
@@ -32,18 +32,21 @@ def create_settings_xml(summaries: ET.Element, peer_root: str, model_root: str) 
 
 """
 
-    model_template = """<M ID="{model_id}">{model_filename}</M>"""
-
-
     for settings_id, chart in enumerate(summaries):
         for eval_id, original in enumerate(chart):
             settings_content = """<ROUGE-EVAL version="1.55">\n"""
-            model = f"""<M ID="{eval_id}">original_{settings_id}_{eval_id}.txt</M>"""
+            # model = f"""<M ID="{eval_id}">original_{settings_id}_{eval_id}.txt</M>"""
+            models = []
+            for idx in range(23):
+                models.append(f"""<M ID="{idx}">original_{settings_id}_{idx}.txt</M>""")
+            model = '\n'.join(models)
+
             peers = []
             for peer_id, gpt2 in enumerate(original):
                 peers.append(f"""<P ID="{settings_id}_{eval_id}_{peer_id}">gpt2_{settings_id}_{eval_id}_{peer_id}.txt</P>""")
-            
-            settings_content += template.format(eval_id=eval_id, peer_root=peer_root, model_root=model_root, peers="\n".join(peers), models=model)
+            peer = '\n'.join(peers)
+
+            settings_content += template.format(eval_id=eval_id, peer_root=peer_root, model_root=model_root, peers=peer, models=model)
     
             settings_content += """</ROUGE-EVAL>\n"""
             with open(f"../summarizer-master/rouge/gpt2_results/settings_{settings_id}_{eval_id}.xml", "w+") as settings:
@@ -52,34 +55,21 @@ def create_settings_xml(summaries: ET.Element, peer_root: str, model_root: str) 
 
 
 if __name__ == '__main__':
-    if "jonas" in sys.argv:
-        create_settings_xml(ET.parse('gpt_output.xml').getroot(), "./gpt2_results/systems/", "./gpt2_results/models/")
-        sys.exit(0)
-
     summaries = ET.parse('gpt_output.xml').getroot()
-    #print(summaries)
+    create_settings_xml(summaries, "./gpt2_results/systems/", "./gpt2_results/models/")
     for chart_id, chart in enumerate(summaries):
         original_counter: int = 0
-        #print(chart.tag, chart.attrib)
-        for attr in chart.attrib.values():
-            #print('yey')
+        for original in chart:
+            gpt2_counter: int = 0
+            #print(original.tag, original.attrib)
+            original_filename = 'original_' + str(chart_id) + '_' + str(original_counter)
+            print("of=",original_filename)
+            create_human_html(cast(str,original.text), original_filename)
             
-            #TODO: do the same for all attr once I figure out how it works
-            if attr == 'what_do_students_choose_to_study.txt\n':
-                for original in chart:
-                    gpt2_counter: int = 0
-                    #print(original.tag, original.attrib)
-                    original_filename = 'original_' + str(chart_id) + '_' + str(original_counter)
-                    print("of=",original_filename)
-                    create_human_html(cast(str,original.text), original_filename)
-                    
-                    for gpt2 in original:
-                        #print(gpt2.tag, gpt2.attrib)
-                        gpt2_filename = 'gpt2_' + str(chart_id) + '_' + str(original_counter) + '_' + str(gpt2_counter)
-                        print("gf=", gpt2_filename)
-                        create_gpt2_html(cast(str,gpt2.text), gpt2_filename)
-                        gpt2_counter += 1
-                    original_counter += 1
-        
-    
-    
+            for gpt2 in original:
+                #print(gpt2.tag, gpt2.attrib)
+                gpt2_filename = 'gpt2_' + str(chart_id) + '_' + str(original_counter) + '_' + str(gpt2_counter)
+                print("gf=", gpt2_filename)
+                create_gpt2_html(cast(str,gpt2.text), gpt2_filename)
+                gpt2_counter += 1
+            original_counter += 1    
