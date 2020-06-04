51c28b862bba6c5a7363a9d7ad803f48cf9b37d5
Simina Cotop <simina.cotop@gmail.com>
2020-06-04 22:42:15 +0200

    Parse chart files to get annotations (part2)

--- model8/gen_few_shot_files_sentences.py
+++ model8/gen_few_shot_files_sentences.py
@@ -1,7 +1,7 @@
 # Generate input files for the chartsopt2 domain, from the training data, i.e. chart descriptions given by humans
 # including descriptions of the bars as sentences
 
-from typing import List, Dict, Tuple, NamedTuple
+from typing import List, Dict, Tuple, NamedTuple, Set
 import matplotlib.pyplot as plt
 import os
 import glob
@@ -42,9 +42,20 @@ def parse_info_files_per_chart(chart_file: str) -> Dict[int, List[Tuple[str,str]
                 inline = fin.readline()
                 
     pprint(chart_descriptions)
-    print(idx)
+    #print(idx)
     
     return chart_descriptions
+
+def turn_dict_into_opt_b(chart_descs: Dict[int, List[Tuple[str,str]]]) -> Dict[int, Dict[str, Set[str]]]:
+    chart_infos: Dict[int, Dict[str, Set[str]]] = {}
+    for idx, val in chart_descs.items():
+        chart_infos.setdefault(idx, {})
+        for el in val:
+            proc_idx = el[1].lstrip(" <").rstrip("> ")
+            chart_infos[idx].setdefault(proc_idx, set()).add(el[0])
+    pprint(chart_infos)
+    return chart_infos
+
 
 def parse_info_files_all_charts(chart) -> Tuple[Dict[str, List[Tuple[int, str]]], Dict[str, List[Tuple[int, str]]], Dict[str, List[Tuple[int, List[str]]]],Dict[str, List[Tuple[int, List[str]]]]]:
     x_categories: Dict[str, List[Tuple[int, str]]] = {}
@@ -99,29 +110,42 @@ def parse_info_files_all_charts(chart) -> Tuple[Dict[str, List[Tuple[int, str]]]
     return x_categories, y_values, differences_add, differences_mul
 
 
-def generate_files(x_categories: Dict[str, List[Tuple[int, str]]], y_values: Dict[str, List[Tuple[int, str]]], diff_add: Dict[str, List[Tuple[int, List[str]]]], diff_mul: Dict[str, List[Tuple[int, List[str]]]]) -> str:
-    chart_line: str = ""
+def generate_files_opt_b(chart_infos: Dict[int, Dict[str, Set[str]]]) -> List[str]:
+    all_chart_lines: List[str] = []
     global_counter: int = 1
-    for all_values_xcat, all_values_yval in zip(x_categories.values(), y_values.values()):
-        for (idx1, val1), (idx2, val2) in zip(all_values_xcat, all_values_yval):
-            chart_line += f"bar{idx1+1}_x_categories_{global_counter}:{val1}\tbar{idx1+1}_y_values_{global_counter}:{val2}\t" 
-            for all_values_diffadd in diff_add.values():
-                for (idx3, val3) in all_values_diffadd:
-                    if val1 in val3:
-                        local_counter:int = 0
-                        for el in val3:
-                            chart_line += f"bar{idx1+1}_diffadd{idx3}_{global_counter+local_counter}:{el}\t"
-                            local_counter += 1
-            for all_values_diffmul in diff_mul.values():
-                for (idx4, val4) in all_values_diffmul:
-                    if val1 in val4:
-                        local_counter:int = 0
-                        for el in val4:
-                            chart_line += f"bar{idx1+1}_diffmul{idx4}_{global_counter+local_counter}:{el}\t"
-                            local_counter += 1
-    chart_line += "\n"
-    print(chart_line)
-    return chart_line
+    # idx: int, all_vals: Dict[str, Set[str]]
+    for idx, all_vals in chart_infos.items():
+        chart_line: str = ""
+        # dict_idx: str, elems: Set[str]
+        for dict_idx, elems in all_vals.items():
+            # If the set has only one element
+            if len(elems) == 1:
+                # Store that one element
+                element: str = list(elems)[0]
+                all_words_in_element: List[str] = element.split(" ")
+                # If element contains one word
+                '''if len(all_words_in_element) == 1:
+                    chart_line += f"{dict_idx}_1:{element}\t"
+                else:'''
+                for word_idx, word in enumerate(all_words_in_element):
+                    chart_line += f"{dict_idx}_{word_idx+1}:{word}\t"
+            else: # The set has more than one element
+                for set_idx, el in enumerate(elems):
+                    all_words_in_element: List[str] = el.split(" ")    
+                    # If each element consists of only ONE word
+                    '''if len(all_words_in_element) == 1:
+                        chart_line += f"{dict_idx}_{set_idx+1}:{el}\t" 
+                    else: # if the line contains more than one word
+                    '''
+                    for widx, wel in enumerate(all_words_in_element):
+                        chart_line += f"{dict_idx}_{widx+1}:{wel}\t" 
+
+        chart_line += "\n"
+        all_chart_lines.append(chart_line)
+    for idx, el in enumerate(all_chart_lines):
+        print(idx, el, "\n")
+    return all_chart_lines
+
 
 #TODO: add list of no_delexi data files
 if __name__ == "__main__":
@@ -129,7 +153,10 @@ if __name__ == "__main__":
     #all_charts = ['info_gender_pay_gap.txt']
     no_delexi_charts=['women_representation_in_different_sectors.txt']
     chart_descs = parse_info_files_per_chart(no_delexi_charts[0])
-    print(chart_descs)
+    #print(chart_descs)
+    #chart_infos_a = turn_dict_into_opt_a(chart_descs)
+    chart_infos_b = turn_dict_into_opt_b(chart_descs)
+    generate_files_opt_b(chart_infos_b)
     '''with open('chartsopt1.box', 'w') as g:
         for chart in all_charts:
             xcat, yvals, diffadd, diffmul = parse_info_files_all_charts(chart)
