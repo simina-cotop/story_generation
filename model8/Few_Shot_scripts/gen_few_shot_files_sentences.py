414f3abf353e46b6a20af6be319839a6a3b1dfce
Simina Cotop <simina.cotop@gmail.com>
2020-06-15 15:18:15 +0200

    Organise code and store sentences instead of entire descriptions

--- model8/Few_Shot_scripts/gen_few_shot_files_sentences.py
+++ model8/Few_Shot_scripts/gen_few_shot_files_sentences.py
@@ -11,7 +11,6 @@ from pprint import pprint
 # Each line in the file will look like this:
 # global description + local description, where the local one will be 23 different ones
 
-# TODO:
 # 1. Parse the info files to get the global information and create a dictionary which has as key each bar basic info 
 # 2. Parse the description files to get the sentences corresponding to each bar and add those to the dictionary
 # 3. From the dictionary generate the files according to the correct structure
@@ -116,65 +115,124 @@ def generate_files_opt_b(chart_infos: Dict[int, Dict[str, Set[str]]]) -> List[st
     #    print(idx, el, "\n")
     return all_chart_lines
 
+def write_to_file_opta(no_delexi_charts: List[str]):
+    with open(os.path.join('chartsopta/original_data/','chartsopta.box'), 'w') as g:
+        with open(os.path.join('chartsopta/original_data/','train.box'), 'w') as train:
+            with open(os.path.join('chartsopta/original_data/','test.box'), 'w') as test:
+                with open(os.path.join('chartsopta/original_data/','valid.box'), 'w') as valid:
+        
+                    for chart in no_delexi_charts:
+
+                        chart_descs, reversed_chart_descs = parse_info_files_per_chart(chart)
+                        #print(chart_descs)
+
+                        chart_lines_a = generate_files_opt_a(reversed_chart_descs)
+
+                        g.write(''.join(chart_lines_a))
+
+                        for line_idx, chart_line in enumerate(chart_lines_a):
+                            if line_idx in list(range(17)):
+                                #print("train=", desc_idx)
+                                train.write(chart_line)
+                            elif line_idx in list(range(17,20)):
+                                test.write(chart_line)
+                                #print("test=", desc_idx)
+                            elif line_idx in list(range(20,23)):
+                                #print("valid=", desc_idx)
+                                valid.write(chart_line)
+
+
+def write_to_file_optb(no_delexi_charts: List[str]):
+    with open(os.path.join('chartsoptb/original_data/','chartsoptb.box'), 'w') as g:
+        with open(os.path.join('chartsoptb/original_data/','train.box'), 'w') as train:
+            with open(os.path.join('chartsoptb/original_data/','test.box'), 'w') as test:
+                with open(os.path.join('chartsoptb/original_data/','valid.box'), 'w') as valid:
+        
+                    for chart in no_delexi_charts:
+                        chart_descs, _ = parse_info_files_per_chart(chart)
+                        #print(chart_descs)
+
+                        chart_infos_b = turn_dict_into_opt_b(chart_descs)
+                        chart_lines_b = generate_files_opt_b(chart_infos_b)
+
+                        g.write(''.join(chart_lines_b))
+
+                        for line_idx, chart_line in enumerate(chart_lines_b):
+                            if line_idx in list(range(17)):
+                                #print("train=", desc_idx)
+                                train.write(chart_line)
+                            elif line_idx in list(range(17,20)):
+                                test.write(chart_line)
+                                #print("test=", desc_idx)
+                            elif line_idx in list(range(20,23)):
+                                #print("valid=", desc_idx)
+                                valid.write(chart_line)
+
+
+def turn_chart_info_into_sentences(chart_file: str) -> Tuple[Dict[int, Dict[int, List[Tuple[str,str]]]], Dict[int, List[Tuple[str,str]]]]:
+    # chart_descriptions[0] = {0: List[(law firms, <x_axis_label_highest_value>),(highest, <y_axis_highest_value>)], 1:List[()],...}
+    chart_descriptions: Dict[int, Dict[int, List[Tuple[str,str]]]] = {}
+    reversed_chart_descriptions: Dict[int, List[Tuple[str,str]]] = {}
+
+    # idx: description index
+    idx: int = 0
+
+    with open("../no_delexi_data/" + chart_file, 'r') as fin:
+        
+        inline = fin.readline() # Reads ''
+        inline = fin.readline()
+        # While we did not reach the end of the file        
+        sent_idx:int = 0
+        while inline != '':
+            
+            # Initialize the dictionary corresponding to the first description
+            chart_descriptions.setdefault(idx, {})
+            inline = inline.replace("\n", "")
+            assert(inline != '')
+            # Split on the spaces
+            splited_inline = inline.split(' <')
+
+            # If we have annotated lines
+            if (len(splited_inline) == 2):
+                assert(splited_inline[1] != '')
+                chart_descriptions[idx].setdefault(sent_idx, list()).append((splited_inline[0].strip(' '), splited_inline[1].rstrip("> ")))
+                #reversed_chart_descriptions.setdefault(idx, list()).append((splited_inline[1].rstrip("> "), splited_inline[0].strip(' ')))
+
+            if inline == '.':
+                sent_idx += 1
+            
+            if inline == '<end_of_description>':
+                    idx += 1
+                    sent_idx = 0
+                    inline = fin.readline()
+            else:
+                inline = fin.readline()
+                
+    pprint(chart_descriptions)
+    #print(idx)
+    
+    return chart_descriptions, reversed_chart_descriptions
+
+
 
 if __name__ == "__main__":
 
-    no_delexi_charts = ['women_representation_in_different_sectors.txt', 'gender_pay_gap.txt', 'how_do_young_people_spend_their_evenings.txt', 'Median_salary_of_women.txt', 'median_salary_per_year_for_se_with_respect_to_their_degrees.txt', 'Money_spent_on_higher_education.txt', 'Number_of_top_Unis.txt', 'what_causes_obesity.txt', 'what_do_students_choose_to_study.txt', 'women_representation_in_different_departments.txt']
-    #no_delexi_charts = ['women_representation_in_different_sectors.txt']
+    #no_delexi_charts = ['women_representation_in_different_sectors.txt', 'gender_pay_gap.txt', 'how_do_young_people_spend_their_evenings.txt', 'Median_salary_of_women.txt', 'median_salary_per_year_for_se_with_respect_to_their_degrees.txt', 'Money_spent_on_higher_education.txt', 'Number_of_top_Unis.txt', 'what_causes_obesity.txt', 'what_do_students_choose_to_study.txt', 'women_representation_in_different_departments.txt']
+    no_delexi_charts = ['women_representation_in_different_sectors.txt']
+   
+    # Create opta files
+    ##write_to_file_opta(no_delexi_charts)
 
-    chart_descs, reversed_chart_descs = parse_info_files_per_chart(no_delexi_charts[0])
-    chart_lines_b = turn_dict_into_opt_b(chart_descs)
+    # Create optb files
+    ##write_to_file_optb(no_delexi_charts)
+
+    #Create chartssenta files
+    turn_chart_info_into_sentences(no_delexi_charts[0])
+
+    #Create chartssentb files
+        
+
+
+
     
-    '''with open(os.path.join('chartsopta/original_data/','chartsopta.box'), 'w') as g:
-        with open(os.path.join('chartsopta/original_data/','train.box'), 'w') as train:
-                with open(os.path.join('chartsopta/original_data/','test.box'), 'w') as test:
-                    with open(os.path.join('chartsopta/original_data/','valid.box'), 'w') as valid:
-            
-                        for chart in no_delexi_charts:
-
-                            chart_descs, reversed_chart_descs = parse_info_files_per_chart(chart)
-                            #print(chart_descs)
-
-                            chart_lines_a = generate_files_opt_a(reversed_chart_descs)
-
-                            g.write(''.join(chart_lines_a))
-
-                            for line_idx, chart_line in enumerate(chart_lines_a):
-                                if line_idx in list(range(17)):
-                                    #print("train=", desc_idx)
-                                    train.write(chart_line)
-                                elif line_idx in list(range(17,20)):
-                                    test.write(chart_line)
-                                    #print("test=", desc_idx)
-                                elif line_idx in list(range(20,23)):
-                                    #print("valid=", desc_idx)
-                                    valid.write(chart_line)'''
-
-
-#TODO: uncomment this when I am done generating chartsopta
-'''    with open(os.path.join('chartsoptb/original_data/','chartsoptb.box'), 'w') as g:
-        with open(os.path.join('chartsoptb/original_data/','train.box'), 'w') as train:
-                with open(os.path.join('chartsoptb/original_data/','test.box'), 'w') as test:
-                    with open(os.path.join('chartsoptb/original_data/','valid.box'), 'w') as valid:
-            
-                        for chart in no_delexi_charts:
-                            chart_descs, _ = parse_info_files_per_chart(chart)
-                            #print(chart_descs)
-
-                            chart_infos_b = turn_dict_into_opt_b(chart_descs)
-                            chart_lines_b = generate_files_opt_b(chart_infos_b)
-
-                            g.write(''.join(chart_lines_b))
-
-                            for line_idx, chart_line in enumerate(chart_lines_b):
-                                if line_idx in list(range(17)):
-                                    #print("train=", desc_idx)
-                                    train.write(chart_line)
-                                elif line_idx in list(range(17,20)):
-                                    test.write(chart_line)
-                                    #print("test=", desc_idx)
-                                elif line_idx in list(range(20,23)):
-                                    #print("valid=", desc_idx)
-                                    valid.write(chart_line)'''
-
     
