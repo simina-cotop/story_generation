bb37e62a0aaf0d8d5b09adf748f1ed3b4ee604bd
Simina Cotop <simina.cotop@gmail.com>
2020-06-28 23:34:22 +0200

    Finish domain chartssb

--- model8/Few_Shot_scripts/gen_semantic_units.py
+++ model8/Few_Shot_scripts/gen_semantic_units.py
@@ -120,25 +120,24 @@ def write_to_file_chartssa(no_delexi_charts: List[str], all_sents: List[List[str
                         chart_descs, reversed_chart_descs = turn_chart_info_into_sentences(chart)
                         
                         #pprint(chart_descs)
-                        print("Original_reversed")
-                        pprint(reversed_chart_descs)
+                        #pprint(reversed_chart_descs)
                         #pprint(all_sents)
 
                         #!!! all_sents must contain a list of sentences, with each sentence being a list of words
                         bigram = Phrases(all_sents, min_count=1, threshold=10)
                         bigram.add_vocab([["Financial", "Groups"], ["Law", "Firms"], ["Computer", "Science"]])
-                        print("aaaaaaaa=",bigram[['Financial', 'Groups', 'are', 'more', 'awsome', 'than', 'law', 'firms', 'and', 'even', 'more', 'awesome', 'than', 'computer', 'science']])
+                        #print("aaaaaaaa=",bigram[['Financial', 'Groups', 'are', 'more', 'awsome', 'than', 'law', 'firms', 'and', 'even', 'more', 'awesome', 'than', 'computer', 'science']])
                         #print(bigram.vocab)
 
-
                         new_reversed_chart_descs = convert_sentences_to_bigrams(reversed_chart_descs, bigram)
-                        print("New_reversed")
-                        pprint(new_reversed_chart_descs)
+                        #pprint(new_reversed_chart_descs)
+
                         # chart_lines_senta : all the sentences belonging to chart `chart`
                         chart_lines_sa = generate_files_sa(new_reversed_chart_descs)
                         #pprint(chart_lines_sa)
+
                         len_all_chart_sentences = len(chart_lines_sa)
-                        print("len=", len_all_chart_sentences)
+                        #print("len=", len_all_chart_sentences)
                         g.write(''.join(chart_lines_sa))
 
                         for line_idx, chart_line in enumerate(chart_lines_sa):
@@ -153,7 +152,7 @@ def write_to_file_chartssa(no_delexi_charts: List[str], all_sents: List[List[str
                                 train.write(chart_line)
 
 
-'''def turn_dict_into_sent_b(chart_descs: Dict[int, Dict[int, List[Tuple[str,str]]]]) -> Dict[int, Dict[int, Dict[str, Set[str]]]]:
+def turn_dict_into_sent_b(chart_descs: Dict[int, Dict[int, List[Tuple[str,str]]]]) -> Dict[int, Dict[int, Dict[str, Set[str]]]]:
     chart_infos: Dict[int, Dict[int, Dict[str, Set[str]]]] = {}
     # all_vals: Dict[int, List[Tuple[str,str]]]
     for idx, all_vals in chart_descs.items():
@@ -169,55 +168,64 @@ def write_to_file_chartssa(no_delexi_charts: List[str], all_sents: List[List[str
                     chart_infos[idx][sent_idx][proc_idx].add(el[0])
     print("HERE\n")
     pprint(chart_infos)
-    return chart_infos'''
+    return chart_infos
 
+def convert_chartssb_to_bigrams(chart_infos: Dict[int, Dict[int, Dict[str, Set[str]]]], bigram2) -> Dict[int, Dict[int, Dict[str, List[str]]]]:
+    new_chart_infos: Dict[int, Dict[int, Dict[str, List[str]]]] = {}
+    # all_vals: Dict[int, Dict[str, Set[str]]]
+    for idx, all_vals in chart_infos.items():
+        new_chart_infos.setdefault(idx, {})
+        # sent_idx:int, all_sents: Dict[str, Set[str]]
+        for sent_idx, all_sents in all_vals.items():
+            new_chart_infos[idx].setdefault(sent_idx, {})
+            # a_idx: str, all_sents: Set[str]
+            for a_idx, all_set in all_sents.items():
+                new_chart_infos[idx][sent_idx].setdefault(a_idx,list())
+                for el in all_set:
+                    el = el.replace(",", "")
+                    new_chart_infos[idx][sent_idx][a_idx] += bigram2[el.split(" ")]
+    print("HERE3\n")
+    pprint(new_chart_infos)
+    return new_chart_infos
 
-'''def generate_files_sent_b(chart_infos: Dict[int, Dict[int, Dict[str, Set[str]]]]) -> List[str]:
-    print("HERE2\n")
+def generate_files_sb(chart_infos: Dict[int, Dict[int, Dict[str, List[str]]]]) -> List[str]:
+    #print("HERE2\n")
     all_chart_lines: List[str] = []
     global_counter: int = 1
-    # idx: int, all_vals: Dict[int, Dict[str, Set[str]]]
+    # idx: int, all_vals: Dict[int, Dict[str, List[str]]]
     for idx, all_vals in chart_infos.items():
-        # sent_idx: int, all_sents: Dict[int, Set[str]]
+        # sent_idx: int, all_sents: Dict[str, List[str]]]
         for sent_idx, all_sents in all_vals.items():
             chart_line: str = ""
-            # dict_idx: str, elems: Set[str]
+            # dict_idx: str, elems: List[str]
             for dict_idx, elems in all_sents.items():
-                # If the set has only one element
-                if len(elems) == 1:
-                    # Store that one element
-                    element: str = list(elems)[0]
-                    all_words_in_element: List[str] = element.split(" ")
-                    # Iterate over the words in a sentence and output them
-                    for word_idx, word in enumerate(all_words_in_element):
+                    for word_idx, word in enumerate(elems):
                         chart_line += f"{dict_idx}_{word_idx+1}:{word}\t"
-                else: # The set has more than one element
-                    for set_idx, el in enumerate(elems):
-                        all_words_in_element: List[str] = el.split(" ")    
-                        # Iterate over the words in a sentence and output them
-                        for widx, wel in enumerate(all_words_in_element):
-                            chart_line += f"{dict_idx}_{widx+1}:{wel}\t" 
             chart_line = chart_line.rstrip('\t')
             chart_line += "\n"
             all_chart_lines.append(chart_line)
-    #for idx, el in enumerate(all_chart_lines):
-    #    print(idx, el, "\n")
-    return all_chart_lines'''
+    for idx, el in enumerate(all_chart_lines):
+        print(idx, el, "\n")
+    return all_chart_lines
 
 
-'''def write_to_file_chartssentb(no_delexi_charts: List[str]) -> None:
-    with open(os.path.join('chartssentb/original_data/','chartssentb.box'), 'w') as g:
-        with open(os.path.join('chartssentb/original_data/','train.box'), 'w') as train:
-            with open(os.path.join('chartssentb/original_data/','test.box'), 'w') as test:
-                with open(os.path.join('chartssentb/original_data/','valid.box'), 'w') as valid:
+def write_to_file_chartssb(no_delexi_charts: List[str], all_sents: List[List[str]]) -> None:
+    with open(os.path.join('chartssb/original_data/','chartssb.box'), 'w') as g:
+        with open(os.path.join('chartssb/original_data/','train.box'), 'w') as train:
+            with open(os.path.join('chartssb/original_data/','test.box'), 'w') as test:
+                with open(os.path.join('chartssb/original_data/','valid.box'), 'w') as valid:
         
                     for chart in no_delexi_charts:
 
                         chart_descs, _ = turn_chart_info_into_sentences(chart)
                         #print(chart_descs)
 
+                        bigram2 = Phrases(all_sents, min_count=1, threshold=2)
+                        bigram2.add_vocab([["Financial", "Groups"], ["Law", "Firms"], ["Computer", "Science"]])
+                        print("vocab=",bigram2.vocab)
                         chart_infos_sentb = turn_dict_into_sent_b(chart_descs)
-                        chart_lines_sentb = generate_files_sent_b(chart_infos_sentb)
+                        new_infos = convert_chartssb_to_bigrams(chart_infos_sentb, bigram2)
+                        chart_lines_sentb = generate_files_sb(new_infos)
                         len_all_chart_sentences = len(chart_lines_sentb)
                         print("len=", len_all_chart_sentences)
                         g.write(''.join(chart_lines_sentb))
@@ -231,7 +239,7 @@ def write_to_file_chartssa(no_delexi_charts: List[str], all_sents: List[List[str
                                 valid.write(chart_line)
                             elif line_idx in list(range(10,len_all_chart_sentences)):
                                 #print("train=", line_idx)
-                                train.write(chart_line)'''
+                                train.write(chart_line)
 
 
 
@@ -248,10 +256,10 @@ if __name__ == "__main__":
             all_descriptions.append(all_words)
    
     #Create chartssenta files
-    write_to_file_chartssa(no_delexi_charts, all_descriptions)
+    #write_to_file_chartssa(no_delexi_charts, all_descriptions)
 
     #Create chartssentb files
-    #write_to_file_chartssentb(no_delexi_charts)
+    write_to_file_chartssb(no_delexi_charts, all_descriptions)
         
 
 
