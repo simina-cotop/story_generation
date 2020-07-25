ef271479702d89390c8bbddcd5479b6f5449777b
Simina Cotop <simina.cotop@gmail.com>
2020-07-25 11:32:35 +0200

    Update script and organize files

--- model8/Few_Shot_scripts/correctness_check.py
+++ model8/Few_Shot_scripts/correctness_check.py
@@ -2,10 +2,18 @@
 from typing import Dict, List, Tuple
 from pprint import pprint
 
+unit_mapping = {
+    'percentage': ['%'],
+    'salary(1000$)': ['1000', '$'],
+    ' (million dollar)': ['1000000'],
+    'number of university': ['#'],
+    'percentage of people': ['%']
+}
+
 
 #!!!TODO:Change this to test once I have the results
-#!!!TODO:Do not need to pas the chart as argument because the file contains ALL charts
-def get_original_generated_sentences_opt_domain(domain: str, chart: str, domain_outputs: str) -> None:
+#!!!TODO:Do not need to pass the chart as argument because the file contains ALL charts
+def get_original_generated_sentences_opt_domain(domain: str, chart: str, domain_outputs: str) -> Dict[str, Dict[str, List[Tuple[str,str]]]]:
     #Dict[domain, Dict[chart, List[Tuple[original, generated]]]]
     all_sentences: Dict[str, Dict[str, List[Tuple[str,str]]]] = {}
     all_sentences.setdefault(domain, {})
@@ -16,6 +24,7 @@ def get_original_generated_sentences_opt_domain(domain: str, chart: str, domain_
                 all_sentences[domain][chart].append((orig_line.strip(' \n'), gen_line))
     #for el in all_sentences[domain][chart]:
     #    print(el[0] + '\t' + el[1] + '\n\n\n')
+    return all_sentences
 
 
 # Parse the charts_info files and get the pairs containg the x categories and the y values, i.e. turn 
@@ -23,37 +32,72 @@ def get_original_generated_sentences_opt_domain(domain: str, chart: str, domain_
 #y values : 20, 60, 10, 70, 30
 # into
 # (Computer Science, 20), (Arts, 60), (Mathematics, 10), (Literature, 70), (Engineering, 30)
-def get_chart_labels(all_charts: List[str]) -> Dict[str, List[Tuple[str,str]]]:
+def get_chart_labels(all_charts: List[str]) -> Dict[str, Dict[str, List[Tuple[str,str]]]]:
     
     #Dict[chart,List[Tuple[x_cat_val1,y_val1],Tuple[x_cat_val2,y_val2]...]]
-    all_info: Dict[str, List[Tuple[str,str]]] = {}
+    all_info: Dict[str, Dict[str,List[Tuple[str,str]]]] = {}
     
     for chart_file in all_charts:
-        all_info.setdefault(chart_file,list())
+        all_info.setdefault(chart_file,{})
 
         chart_info : Dict[str, List[str]] = {}
 
         with open(f'../charts_info/{chart_file}','r') as f:
             data = f.read()
         lines = data.split("\n")
-        # Kick out all the other lines except the first 2
-        lines = lines[:3]
-        # Kick out the title
-        lines = lines[1:]
-        for line in lines:
-            aux = line.split(" : ")
-            # Category
-            processed_aux: str = aux[1].lower()
-            # Actual values
-            processed_aux = processed_aux.split(", ")
-            chart_info[aux[0]] = processed_aux
+        relevant_lines: List[str] = []
+
+        # Save only the lines we need 
+        relevant_lines.append(lines[1]) # x categories
+        relevant_lines.append(lines[2]) # y values
+        relevant_lines.append(lines[5]) # y axis unit
+        
+        for line_idx, line in enumerate(relevant_lines):
+            if line_idx == 0 or line_idx == 1:
+                aux = line.split(" : ")
+                # Category
+                processed_aux: str = aux[1].lower()
+                # Actual values
+                processed_aux = processed_aux.split(", ")
+                chart_info[aux[0]] = processed_aux
+            else:
+                aux = line.split(" : ")
+                chart_info[aux[0]] = []
+                chart_info[aux[0]].append(aux[1].lower())
+
+        for unit in chart_info['y axis unit']:
+            all_info[chart_file][unit] = []
+
+        pprint(chart_info)
 
         # Save pairs of (xcat,yval)
-        for xcat, yval in zip(chart_info['x categories'], chart_info['y values']):
-            all_info[chart_file].append((xcat,yval))
+        for unit in chart_info['y axis unit']:
+            for xcat, yval in zip(chart_info['x categories'], chart_info['y values']):
+                all_info[chart_file][unit].append((xcat,yval))
             
-    #pprint(all_info)
+    pprint(all_info)
     return all_info
+
+#TODO: integrate the units
+#Takes as input the chart and its corresponding label information and checks if that information appears correctly in the generated text
+def check1(chart: str, chart_labels:List[Tuple[str,str]], all_sentences: Dict[str, Dict[str, List[Tuple[str,str]]]], units_maps: List[str]) -> None:
+    #pprint(all_sentences)
+    one_pair = all_sentences['chartsopta']['gender_pay_gap.txt'][3]
+    print(one_pair, chart_labels)
+    processed_original: List[str] = one_pair[0].lower().split(" ")
+    processed_generated: List[str] = one_pair[1].lower().split(" ")
+    for label in chart_labels:
+        if label[0] in processed_generated:
+            print(label[0], processed_generated)
+            index = processed_generated.index(label[0])
+            print("index=", index)
+            for window in range(1,4):
+                sliced_sent = processed_generated[index:index+window]
+                #TODO: will probably need to change this when I incorporate the units
+                for el in sliced_sent:
+                    if label[1] in el:
+                        print(sliced_sent)
+
     
 
 
@@ -68,5 +112,7 @@ if __name__ == '__main__':
     domain_outputs = ['../../../../output_files_aws/20200627223805/chartsopta/results/loads/23/23/valid/']
     no_delexi_charts = ['women_representation_in_different_sectors.txt', 'gender_pay_gap.txt', 'how_do_young_people_spend_their_evenings.txt', 'Median_salary_of_women.txt', 'median_salary_per_year_for_se_with_respect_to_their_degrees.txt', 'Money_spent_on_higher_education.txt', 'Number_of_top_Unis.txt', 'what_causes_obesity.txt', 'what_do_students_choose_to_study.txt', 'women_representation_in_different_departments.txt']
     info_charts = ['info_gender_pay_gap.txt', 'info_median_salary_women.txt', 'info_young_evenings.txt', 'info_median_salary_se.txt', 'info_women_study_departments.txt', 'info_num_top_unis.txt', 'info_obesity.txt', 'info_women_work_sector.txt', 'info_student_choice_study.txt', 'info_money_spent_he.txt']
-    get_original_generated_sentences_opt_domain(domains[0], no_delexi_charts[0], domain_outputs[0])
+
+    all_sentences = get_original_generated_sentences_opt_domain(domains[0], no_delexi_charts[1], domain_outputs[0])
     chart_labels = get_chart_labels(info_charts)
+    check1(no_delexi_charts[1], chart_labels['info_gender_pay_gap.txt']['percentage'], all_sentences, unit_mapping['percentage'])
